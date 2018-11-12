461a09f48e819c42435545460987d0e485a2bc6e
Elder Martin Martiez Carrillo <eldermcarrillo@gmail.com>
2018-11-11 23:22:55 -0600

    refactor: update_content

--- src/Containers/Markers/content.js
+++ src/Containers/Markers/content.js
@@ -12,8 +12,8 @@ import isEmpty from 'lodash/isEmpty';
 
 import GoogleMapReact from 'google-map-react';
 
-import { Map, InfoWindow, Marker, GoogleApiWrapper, Polygon } from 'google-maps-react';
-import { Map as Mappoly ,Polygon as Polygon2 ,Polyline } from 'google-maps-react';
+import { Map, Marker, GoogleApiWrapper, Polygon } from 'google-maps-react';
+import { Map as Mappoly, Polygon as Polygon2, Polyline, InfoWindow } from 'google-maps-react';
 
 import { postRequest, getRequest, getlistRequest } from '../../actions/httpActions';
 import { addFlashMessageModal } from '../../actions/flashMessages';
@@ -27,6 +27,9 @@ import "react-table/react-table.css";
 var inside = require('point-in-polygon');
 
 import map from 'lodash/map'
+
+import Paper from 'material-ui/Paper';
+import { typography } from 'material-ui/styles';
 
 class Markers extends React.Component {
 
@@ -57,7 +60,11 @@ class Markers extends React.Component {
       lngp: '',
       namep: '',
       datap: [],
-      dataline: []
+      dataline: [],
+
+      showingInfoWindow: false,
+      activeMarker: {},
+      selectedPlace: {},
     }
 
     this.onChange = this.onChange.bind(this);
@@ -68,7 +75,7 @@ class Markers extends React.Component {
 
     this.onClick = this.onClick.bind(this);
 
-    this.onMarkerClick = this.onMarkerClick.bind(this);
+    // this.onMarkerClick = this.onMarkerClick.bind(this);
     this.onMapClicked = this.onMapClicked.bind(this);
 
     this.datamarkers = this.datamarkers.bind(this);
@@ -88,11 +95,11 @@ class Markers extends React.Component {
     this.setState({ [e.target.name]: e.target.value });
   }
   onSubmit(e) {
-   
+
     e.preventDefault();
-    if(e.target.name=="btn-guardar-poligono"){
-       this.POST('new_polygon', 'markers');
-    }else{
+    if (e.target.name == "btn-guardar-poligono") {
+      this.POST('new_polygon', 'markers');
+    } else {
       if (this.isValid()) {
         this.POST('new_marker', 'markers');
       }
@@ -110,9 +117,9 @@ class Markers extends React.Component {
     if (Validator.isEmpty(String(e.lng))) {
       errors.lng = 'Este campo es requerido';
     }
-  /*  if (Validator.isEmpty(String(e.namep))) {
-      errors.e.namep = 'Este campo es requerido';
-    }*/
+    /*  if (Validator.isEmpty(String(e.namep))) {
+        errors.e.namep = 'Este campo es requerido';
+      }*/
     return {
       errors,
       isValid: isEmpty(errors)
@@ -140,27 +147,35 @@ class Markers extends React.Component {
       array_polys[0][item].map(function (item, i) {
         array_pol.push([item.lat, item.lng]);
       })
-      console.log(array_pol);
-      console.log(inside([e.lat, e.lng], array_pol));
     })
 
   }
-  onMarkerClick(props, marker, e) {
-    // ..
-  }
+  onMarkerClick = (props, marker, e) =>
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
 
   onMapClicked(props, map, e) {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      })
+    };
+
+
     var arrayp = [];
-    this.state.datap.push({lat : e.latLng.lat(), lng : e.latLng.lng()});
+    this.state.datap.push({ lat: e.latLng.lat(), lng: e.latLng.lng() });
 
     arrayp = this.state.datap;
 
     this.setState({
-      latp : e.latLng.lat(),
-      lngp : e.latLng.lng(),
-      datap : arrayp
+      latp: e.latLng.lat(),
+      lngp: e.latLng.lng(),
+      datap: arrayp
     })
-    console.log(this.state.namep);
   }
   refreshTable(e) {
     this.childTablePhrases.filterAll(e);
@@ -236,17 +251,17 @@ class Markers extends React.Component {
   datamarkers(data) {
     this.setState({
       datadt: data,
-      dataline :[]
+      dataline: []
     });
 
-   var array_line = [];
+    var array_line = [];
 
-   data.map(function (item, i) {
-    array_line.push({lat:parseFloat(item.lat),lng: parseFloat(item.lng)});
-   });
-   this.setState({
-    dataline :array_line
-   });
+    data.map(function (item, i) {
+      array_line.push({ lat: parseFloat(item.lat), lng: parseFloat(item.lng) });
+    });
+    this.setState({
+      dataline: array_line
+    });
   }
   pointpolygon(row) {
     var array_polys = this.state.polygonCoords;
@@ -267,7 +282,7 @@ class Markers extends React.Component {
   }
   render() {
     const { errors } = this.state;
-    const poly = this.state.nom_poly, polys = this.state.polygonCoords ,poly2 =this.state.datap,polyline = this.state.dataline;
+    const poly = this.state.nom_poly, polys = this.state.polygonCoords, poly2 = this.state.datap, polyline = this.state.dataline;
     const columns = [
       {
         Header: () => <b>Name</b>,
@@ -304,179 +319,175 @@ class Markers extends React.Component {
         <div className="col-md-12">
           <Tabs activeKey={this.state.key} onSelect={this.handleSelect} id="uncontrolled-tab-example" bsStyle="tabs">
             <Tab eventKey={1} title="Markers">
-              <div className="container">
-                <div className="row">
-                  <div className="col-md-12" style={{ paddingTop: '10px' }}>
-                    <div className="col-md-4">
-                      <div className={classnames("form-group", { 'has-error': errors.marker })}>
-                        <TextFieldGroup
-                          error={errors.marker}
-                          label="Nombre"
-                          onChange={this.onChange}
-                          value={this.state.marker}
-                          field="marker"
-                          placeHolder=""
-                          required="true"
-                        />
-                      </div>
+              <div className="row">
+                <div className="col-md-12" style={{ paddingTop: '10px' }}>
+                  <div className="col-md-4">
+                    <div className={classnames("form-group", { 'has-error': errors.marker })}>
+                      <TextFieldGroup
+                        error={errors.marker}
+                        label="Nombre"
+                        onChange={this.onChange}
+                        value={this.state.marker}
+                        field="marker"
+                        placeHolder=""
+                        required="true"
+                      />
                     </div>
-                    <div className="col-md-4">
-                      <div className={classnames("form-group", { 'has-error': errors.lat })}>
-                        <TextFieldGroup
-                          error={errors.lat}
-                          label="Latitud"
-                          onChange={this.onChange}
-                          value={this.state.lat}
-                          field="lat"
-                          placeHolder=""
-                          required="true"
-                        />
-                      </div>
-                    </div>
-                    <div className="col-md-4">
-                      <div className={classnames("form-group", { 'has-error': errors.lng })}>
-                        <TextFieldGroup
-                          error={errors.lng}
-                          label="Longitud"
-                          onChange={this.onChange}
-                          value={this.state.lng}
-                          field="lng"
-                          placeHolder=""
-                          required="true"
-                        />
-                      </div>
-                    </div>
-                    <button disabled={this.state.isLoading} onClick={this.onSubmit} className="btn btn-primary pull-right">Guardar</button>
                   </div>
-                </div>
-                <div className="row" style={{ paddingTop: '10px' }}>
-                  <div className="col-md-12">
-                    <div style={{ height: '60vh', width: '100%' }}>
-                      <GoogleMapReact
-                        bootstrapURLKeys={{ key: this.props.key }}
-                        defaultCenter={this.props.center}
-                        defaultZoom={this.props.zoom}
-                        onClick={this.onClick}
-                      >
-                        <Markermap
-                          key='1'
-                          lat={this.lat}
-                          lng={this.lng}
-                          text={'new point'}
-                        />
-                      </GoogleMapReact>
+                  <div className="col-md-4">
+                    <div className={classnames("form-group", { 'has-error': errors.lat })}>
+                      <TextFieldGroup
+                        error={errors.lat}
+                        label="Latitud"
+                        onChange={this.onChange}
+                        value={this.state.lat}
+                        field="lat"
+                        placeHolder=""
+                        required="true"
+                      />
                     </div>
+                  </div>
+                  <div className="col-md-4">
+                    <div className={classnames("form-group", { 'has-error': errors.lng })}>
+                      <TextFieldGroup
+                        error={errors.lng}
+                        label="Longitud"
+                        onChange={this.onChange}
+                        value={this.state.lng}
+                        field="lng"
+                        placeHolder=""
+                        required="true"
+                      />
+                    </div>
+                  </div>
+                  <button disabled={this.state.isLoading} onClick={this.onSubmit} className="btn btn-primary pull-right">Guardar</button>
+                </div>
+              </div>
+              <div className="row" style={{ paddingTop: '10px' }}>
+                <div className="col-md-12">
+                  <div style={{ height: '60vh', width: '100%' }}>
+                    <GoogleMapReact
+                      bootstrapURLKeys={{ key: this.props.key }}
+                      defaultCenter={this.props.center}
+                      defaultZoom={this.props.zoom}
+                      onClick={this.onClick}
+                    >
+                      <Markermap
+                        key='1'
+                        lat={this.lat}
+                        lng={this.lng}
+                        text={'new point'}
+                      />
+                    </GoogleMapReact>
                   </div>
                 </div>
               </div>
             </Tab>
             <Tab eventKey={2} title="Poligonos">
-              <div className="container">
-                <div className="row">
-                  <div className="col-md-12" style={{ paddingTop: '10px' }}>
-                    <div className="col-md-4">
-                      <div className={classnames("form-group", { 'has-error': errors.namep })}>
-                        <TextFieldGroup
-                          error={errors.namep}
-                          label="Nombre Figura"
-                          onChange={this.onChange}
-                          value={this.state.namep}
-                          field="namep"
-                          placeHolder=""
-                          required="true"
-                        />
-                      </div>
+              <div className="row">
+                <div className="col-md-12" style={{ paddingTop: '10px' }}>
+                  <div className="col-md-4">
+                    <div className={classnames("form-group", { 'has-error': errors.namep })}>
+                      <TextFieldGroup
+                        error={errors.namep}
+                        label="Nombre Figura"
+                        onChange={this.onChange}
+                        value={this.state.namep}
+                        field="namep"
+                        placeHolder=""
+                        required="true"
+                      />
                     </div>
-                    <div className="col-md-4">
-
-                    </div>
-                    <div className="col-md-4">
-
-                    </div>
-                    <button name="btn-guardar-poligono" disabled={this.state.isLoading} onClick={this.onSubmit} className="btn btn-primary pull-right">Guardar</button>
                   </div>
+                  <div className="col-md-4">
+
+                  </div>
+                  <div className="col-md-4">
+
+                  </div>
+                  <button name="btn-guardar-poligono" disabled={this.state.isLoading} onClick={this.onSubmit} className="btn btn-primary pull-right">Guardar</button>
                 </div>
-                <div className="row">
-                  <div className="col-md-12">
-                    <Mappoly google={this.props.google}
-                      style={{ width: '100%', height: '60vh', position: 'relative' }}
-                      className={'map'}
-                      initialCenter={this.props.center}
-                      zoom={this.props.zoom}
-                      onClick={this.onMapClicked}
-                      
-                      >
-                      {
-                          poly2.map(function (item, i) {
-                              return <Polyline
-                              key={i}
-                              path={poly2}
-                              strokeColor="#0000FF"
-                              strokeOpacity={0.8}
-                              strokeWeight={2} />
-                          })
-                        }
-                    </Mappoly>
-                  </div>
+              </div>
+              <div className="row">
+                <div className="col-md-12">
+                  <Mappoly google={this.props.google}
+                    style={{ width: '100%', height: '60vh', position: 'relative' }}
+                    className={'map'}
+                    initialCenter={this.props.center}
+                    zoom={this.props.zoom}
+                    onClick={this.onMapClicked}
+
+                  >
+                    {
+                      poly2.map(function (item, i) {
+                        return <Polyline
+                          key={i}
+                          path={poly2}
+                          strokeColor="#0000FF"
+                          strokeOpacity={0.8}
+                          strokeWeight={2} />
+                      })
+                    }
+                  </Mappoly>
                 </div>
               </div>
             </Tab>
             <Tab eventKey={3} title="Geoposición">
-              <div className="container">
-                <div className="row">
-                  <div className="col-md-12" style={{ paddingTop: '10px' }}>
-                    <div className="col-md-6" style={{ height: '30px' }}>
-                      <Table
-                        columns={columns}
-                        controller="Markers"
-                        type="get_markers"
-                        date="false"
-                        excel="false"
-                        ref={ref => this.childTablePhrases = ref}
-                        datamarkers={this.datamarkers}
-                      />
-                    </div>
-                    <div className="col-md-6">
-                      <Map google={this.props.google}
-                        style={{ width: '100%', height: '60vh', position: 'relative' }}
-                        className={'map'}
-                        initialCenter={this.props.center}
-                        zoom={this.props.zoom}>
-                        {
-                          poly.map(function (item, i) {
-                            return <Polygon
-                              key={i}
-                              paths={polys[0][item]}
-                              strokeColor="#0000FF"
-                              strokeOpacity={0.8}
-                              strokeWeight={2}
-                              fillColor="#0000FF"
-                              fillOpacity={0.35}
-                            />
-                          })
-                        }
-                        {
-                          this.state.datadt.map(function (item, i) {
-                            return <Marker
-                              key={i}
-                              title={item.name}
-                              name={'punto'}
-                              text={item.name}
-                              position={{ lat: item.lat, lng: item.lng }} />
-                          })
-                        }
-                        {
-                          polyline.map(function (item, i) {
-                            return  <Polyline
+              <div className="row">
+                <div className="col-md-12" style={{ paddingTop: '10px' }}>
+                  <div className="col-md-6" style={{ height: '30px' }}>
+                    <Table
+                      columns={columns}
+                      controller="Markers"
+                      type="get_markers"
+                      date="false"
+                      excel="false"
+                      ref={ref => this.childTablePhrases = ref}
+                      datamarkers={this.datamarkers}
+                    />
+                  </div>
+                  <div className="col-md-6">
+                    <Map google={this.props.google}
+                      style={{ width: '100%', height: '60vh', position: 'relative' }}
+                      className={'map'}
+                      initialCenter={this.props.center}
+                      zoom={this.props.zoom}>
+                      {
+                        poly.map(function (item, i) {
+                          return <Polygon
                             key={i}
-                            path= {polyline}
+                            paths={polys[0][item]}
+                            strokeColor="#0000FF"
+                            strokeOpacity={0.8}
+                            strokeWeight={2}
+                            fillColor="#0000FF"
+                            fillOpacity={0.35}
+                          />
+                        })
+                      }
+                      {
+                        this.state.datadt.map(function (item, i) {
+                          return <Marker
+                            key={i}
+                            title={item.name}
+                            name={'punto'}
+                            text={item.name}
+                            position={{ lat: item.lat, lng: item.lng }}
+                          //  onClick={this.onMarkerClick}
+                          />
+                        })
+                      }
+                      {
+                        polyline.map(function (item, i) {
+                          return <Polyline
+                            key={i}
+                            path={polyline}
                             strokeColor="#808B96"
                             strokeOpacity={0.8}
                             strokeWeight={2} />
-                          })
-                        }
-                      </Map>
-                    </div>
+                        })
+                      }
+                    </Map>
                   </div>
                 </div>
               </div>
